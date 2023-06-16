8778b23f355d57246670705e94c8809c940cf82e
Kantemir <kantemir.kertiev@hotmail.com>
2023-06-15 23:41:45 -0400

    added id and hash

--- index.py
+++ index.py
@@ -7,8 +7,8 @@ from telethon import errors
 
 # These example values won't work. You must get your own api_id and
 # api_hash from https://my.telegram.org, under API Development.
-api_id = 12345
-api_hash = '0123456789abcdef0123456789abcdef'
+api_id = 20242617
+api_hash = '2b55870c14c32dbd1d4da4636436aeec'
 
 
 async def main():
